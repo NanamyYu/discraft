a532a93033a7de6295030051da601474acb19619
Serezha Rakhmanov <shoraii@hotmail.com>
2022-04-24 20:06:40 +0300

    fix inability to write in chat

--- src/main/java/discraft/discraft/EventListener.java
+++ src/main/java/discraft/discraft/EventListener.java
@@ -5,12 +5,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -22,10 +24,13 @@ public class EventListener implements Listener {
 
     final Database database;
 
+    private HashSet<String> loggedInPlayers;
+
     public EventListener(JavaPlugin plugin, PluginConfig config, Database database) {
         this.plugin = plugin;
         this.config = config;
         this.database = database;
+        this.loggedInPlayers = new HashSet<>();
     }
 
     @EventHandler
@@ -59,18 +64,29 @@ public class EventListener implements Listener {
     }
 
     @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        String name = e.getPlayer().getName();
+        if (this.loggedInPlayers.contains(name)) {
+            this.loggedInPlayers.remove(name);
+        }
+    }
+
+    @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent e) {
         String name = e.getPlayer().getName();
         String message = e.getMessage();
         HashMap<String, String> nameToLoginCode = this.database.getLoginCodesHashMap();
+        if (this.loggedInPlayers.contains(name)) {
+            return;
+        }
         if (!Objects.equals(nameToLoginCode.get(name), "None") && Objects.equals(nameToLoginCode.get(name), message)) {
             e.getPlayer().sendMessage(Constants.MESSAGES.CORRECT_CODE.toString());
             BukkitTask unblockTask = new UnblockTask(e.getPlayer()).runTask(this.plugin);
+            this.loggedInPlayers.add(name);
             this.database.pullLoginCodes(name, "None");
-            e.setCancelled(true);
         } else {
             e.getPlayer().sendMessage(Constants.MESSAGES.INCORRECT_CODE.toString());
-            e.setCancelled(true);
         }
+        e.setCancelled(true);
     }
 }
